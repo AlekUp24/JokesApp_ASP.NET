c74cf1a9fc74f2e2be04b80fc8640241dccbf3ae
AlekUp24 <alekp24@gmail.com>
2024-08-22 13:14:46 +0200

    Done full app

--- JokesApp/Controllers/HomeController.cs
+++ JokesApp/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace JokesApp.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
